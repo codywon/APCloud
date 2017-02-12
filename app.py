4288ba4c4b90868cfee935f3306c53d87712a8a3
Vhyme <vhyme@live.cn>
2017-02-12 12:50:22 +0800

    现在播放器可以动态适应iframe或网页高度了

--- app.py
+++ app.py
@@ -14,16 +14,20 @@ ne = NetEase()
 @app.route("/apcloud/")
 def hello():
     return '''<pre>网易云音乐 APlayer
-项目地址: https://github.com/vhyme/APCloud
-演示效果可以看我的博客(电脑打开): https://heya.myseu.cn/
+项目地址：https://github.com/vhyme/APCloud
+演示效果可以看我的博客(电脑打开)：https://heya.myseu.cn/
 
 Usage:
 /歌单ID       显示指定歌单播放器
-    参数:
-    autoplay     是否自动播放(1=true/0=false), 默认是
-    showlrc      是否显示歌词(1=true/0=false), 默认是
-/歌曲ID.mp3   重定向到该歌曲最高码率直链,若有版权问题则抛出404
+    参数：
+    autoplay     是否自动播放(1=true/0=false)，默认是
+    showlrc      是否显示歌词(1=true/0=false)，默认是
+/歌曲ID.mp3   重定向到该歌曲直链，若有版权问题则返回空音频
 /歌曲ID.lrc   下载该歌曲歌词
+
+本播放器可以自适应窗口或iframe的高度，嵌入到iframe中不会出现
+页面全局滚动条，而是使用APlayer自带的列表滚动条，另外本播放器
+隐藏了APlayer的边距、圆角、外阴影等元素，非常方便嵌入到博客中！
 </pre>'''
 
 
